fe3493a4e7adba47f110dfd57556506d60cf7464
Agung Sundoro <muhagung2001@gmail.com>
2022-01-23 09:10:38 +0700

    Add wp-plugin-svelte-js.php

---
+++ wp-plugin-svelte-js.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Plugin Name:       WordPress Plugin Boilterplate with Svelte
+ * Plugin URI:        https://example.com
+ * Description:       Just another awesome plugin
+ * Version:           1.0.0
+ * Author:            Agung Sundoro
+ * Author URI:        https://example.com
+ * License:           GPL-3.0
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * SOFTWARE LICENSE INFORMATION
+ *
+ * For detailed information regarding to the licensing of
+ * this software, please review the license.txt
+*/
